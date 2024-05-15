045e323e7fbb2ba115f60251b404d565d2d1f304
(Robi) <(r.ruano3@gmail.com)>
2024-05-15 08:53:55 +0200

    ramp up 2 ejercicio 2 final

--- ramp_up_js_2.js
+++ ramp_up_js_2.js
@@ -18,7 +18,7 @@ lo corrijo haciendo el bucle a .length -1 y sumando la última letra a parte. */
 Declara una función que acepte un nombre como parámetro y 
 devuelva el número de palabras que tiene el nombre. Ejemplo: Para "Antonio Alberto Jesús" devolverá 3.*/
 function contarPalabras(nombreCompuesto){
-    let arrayNombre = nombreCompuesto.splite(" ");
+    let arrayNombre = nombreCompuesto.split(" ");
     let respuesta = arrayNombre.length;
     return respuesta;
 };
